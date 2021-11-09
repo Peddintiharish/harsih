1515202d2e07fb3e4dfe9c707549b98024eb1479
Peddintiharish <93182569+Peddintiharish@users.noreply.github.com>
2021-11-09 12:40:36 +0530

    Add files via upload

---
+++ harish1234.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d,p;
+	printf("enter the four numbers");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	p=a*b*c*d;
+	printf("product od %d,%d,%d,%d is: %d", a,b,c,d,p);
+}
